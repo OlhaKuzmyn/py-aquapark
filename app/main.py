a007735456340e1a43bf77718c891e0af5101af1
Olha Kuzmyn <olya.kuzmyn@gmail.com>
2026-03-24 15:02:23 +0200

    solved

--- app/main.py
+++ app/main.py
@@ -1,25 +1,58 @@
-from abc import ABC
-
-
 class IntegerRange:
-    pass
+    def __init__(self, min_amount: int, max_amount: int) -> None:
+        self.min_amount = min_amount
+        self.max_amount = max_amount
+
+    def __set_name__(self, owner, name) -> None:
+        self.protected_name = "_" + name
+
+    def __get__(self, instance, owner) -> int | str:
+        return getattr(instance, self.protected_name)
+
+    def __set__(self, instance, value) -> None:
+        if value not in range(self.min_amount, self.max_amount + 1):
+            raise ValueError()
+        elif not isinstance(value, int):
+            raise TypeError()
+        else:
+            setattr(instance, self.protected_name, value)
 
 
 class Visitor:
-   pass
+    def __init__(self, name: str, age: int, weight: int, height: int) -> None:
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.height = height
 
 
-class SlideLimitationValidator(ABC):
-    pass
+class SlideLimitationValidator:
+    def __init__(self, age: int, weight: int, height: int) -> None:
+        self.age = age
+        self.weight = weight
+        self.height = height
 
 
 class ChildrenSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(4, 14)
+    weight = IntegerRange(20, 50)
+    height = IntegerRange(80, 120)
 
 
 class AdultSlideLimitationValidator(SlideLimitationValidator):
-    pass
+    age = IntegerRange(14, 60)
+    weight = IntegerRange(50, 120)
+    height = IntegerRange(120, 220)
 
 
 class Slide:
-    pass
+    def __init__(self, name: str, limitation_class: SlideLimitationValidator):
+        self.name = name
+        self.limitation_class = limitation_class
+
+    def can_access(self, other: Visitor) -> bool:
+        try:
+            self.limitation_class(age=other.age, weight=other.weight, height=other.height)
+            return True
+        except ValueError or TypeError:
+            return False
